49fdefcc826ee7559f95c33fbb215a0864d42ac8
sunny-lee3 <sunnylee3712@gmail.com>
2021-05-20 22:53:04 -0700

    updated lab 09

--- src/LocationForm.js
+++ src/LocationForm.js
@@ -15,47 +15,65 @@ class LocationForm extends React.Component{
     constructor() {
         super();
     this.state = {
-        data: {},
-        map: {},
+        data: [],
+        map: null,
         weather: {},
-        search: '',  
-        location: {}, 
-        // location: {}, 
-        // map: null,  
-        // weather: null, 
+        search: '',
+        movie: null,  
         }
     }
 
 
 componentDidMount = () => {
     this.getLocation();
-    this.getWeather();
 }     
 getLocation = async (e) => {
-    let response = await axios.get(`http://localhost:3000/location?search=${this.state.search}`);
-    this.setState({
-        data: response.data, 
-    });
+    let response = await axios.get(`http://localhost:3000/location?search=${this.state.search}`)
+    .then(response => {
+        this.setState({data: response.data[0],})
+        this.getWeather() 
+        this.getMap()
+        this.getMovie()
+    })
+    console.log(response); 
 }
 getWeather = async (e) => {
-    let response = await axios.get(`http://localhost:3000/weather?search=${this.state.search}`)
-    this.setState ({
-        weather: response.data, 
-    });
+    let response = await axios.get(`http://localhost:3000/weather?lat=${this.state.data.lat}&lon=${this.state.data.lon}`)
+        this.setState({weather: response.data[0]});
+    console.log(response);
 }
+getMap = async (e) => {
+    let response = await axios.get(`http://localhost:3000/map?lat=${this.state.data.lat}&lon=${this.state.data.lon}`)
+        this.setState({map: response.data[0]})
+}
+getMovie = async (e) => {
+    let response = await axios.get(`http://localhost:3000/movie?search=${this.state.search}`)
+        this.setState({movie: response.data[0]})
+}
+// renderWeather = () => {
+//     let weatherArray = [];
+//     for(let i =0; i<this.state.weather.data; i++){
+//         weatherArray.push(this.state.weather.data[i]); 
+//     }
+//     return weatherArray; 
+// }
 
 render() {
+    console.log(this.state);
     return(
         <>
         <Form>
             <Form.Label>City Explorer</Form.Label>
             <input onChange={(e) => this.setState({search: e.target.value})} placeholder='Enter Location' type='text' />
-            <Button variant='outline-primary' onClick={this.getLocation, this.getWeather}> Get Location </Button> 
+            <Button variant='outline-primary' onClick={this.getLocation}> Get Location </Button> 
             <Form.Text>
             <div>{this.state.data.display_name ? <div>{this.state.data.display_name}, {this.state.data.lat},{this.state.data.lon}</div> : ''}</div>
             </Form.Text>
-            <div>{console.log(this.state.weather)}</div>
             <img src={this.state.map ? this.state.map : null} alt="Location Map" />
+            <Form.Text>
+            <ul>{this.state.weather.date ? <div>{this.state.weather.date}, {this.state.weather.description}</div> : ''}</ul>
+            </Form.Text>
+            
         </Form>
         </>
     );
